ddee8212d4a38a57453edc162eba37eb2f645b8f
Javi Perez <mojocanario7@gmail.com>
2025-01-22 15:40:05 +0000

    Insertada dependencia de Preact para crear una Isla interactiva que se hidrata cada vez que interactuemos con el componente.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        build: {
-            charset: 'utf-8',
-        },
-    },
+  vite: {
+      build: {
+          charset: 'utf-8',
+      },
+  },
+
+  site: "https://galeos-astro-blog.netlify.app/",
+  integrations: [preact()]
 });
